edbb301b8bc2f83b057c26d502936b88c33a156a
Joseph Cutrono <jcutrono@gmail.com>
2022-02-24 19:03:31 -0500

    the end

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static cli.Program;
 
 namespace cli
 {
@@ -6,7 +9,61 @@ namespace cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Func<Person, string> prettyPrint = x => x + Environment.NewLine;
+
+            var people = new List<Person> {
+                new Person { FirstName = "Rebecca", LastName = "Smith", Major = Major.CompSci },
+                new Person { FirstName = "Jay", LastName = "Smith", Major = Major.CompSci },
+                new Person { FirstName = "Susan", LastName = "Smith", Major = Major.CompSci },
+                new Person { FirstName = "Linda", LastName = "Smith", Major = Major.CompSci },
+            };
+
+            Console.WriteLine(CountCompSci(people));
+            Console.WriteLine(people.Count(x => x.Major == Major.CompSci));
+        }
+
+        public static int CountCompSci(IEnumerable<Person> people)
+        {
+            var count = 0;
+            foreach (var person in people)
+            {
+                if (person.Major == Major.CompSci)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }       
+    }
+
+    public enum Major{
+            CompSci,
+            Business,
+            Marketing
+        }
+
+    public class Person
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName { get; set; }
+
+        public Major Major { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hello my name is {FirstName} {LastName}";
         }
     }
+
 }
